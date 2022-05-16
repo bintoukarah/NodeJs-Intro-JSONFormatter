49cc699f96a218dc79c7c21e68c3fffc8bcdb63a
bkaramo <bintoukaramoko3@gmail.com>
2022-05-16 15:43:50 +0200

    Modified dirty files & put process argv

--- json-tidy/magical_program.js
+++ json-tidy/magical_program.js
@@ -1,9 +1,13 @@
 console.log("Hello world");
-const fs = require('fs');
+const fs = require("fs");
 
-const myData = fs.readFileSync("./my_dirty.json",{encoding:'utf-8',flag:'r'});
-let result = JSON.parse(myData);
-let newData = JSON.stringify(result, null, 2 );
-
-fs.writeFileSync("output.json", newData)
+const myData = fs.readFileSync("./my_dirty_example.json", {
+  encoding: "utf-8",
+  flag: "r",
+});
+let result = JSON.parse(myData); // Traduire mes données
+let newData = JSON.stringify(result, null, 2); // Le mettre en forme
+fs.writeFileSync("output.json", newData); // WriteFileSync permet de mettre en forme dans un nouveau fichier (outpout.json) en lisant my dirty.json
 console.log(newData);
+const myArgs = process.argv.slice (2);
+console.log(myArgs);
