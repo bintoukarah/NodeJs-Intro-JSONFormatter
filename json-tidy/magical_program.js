7049bd0b9220435b22c712f3415257627ad37922
bkaramo <bintoukaramoko3@gmail.com>
2022-05-16 17:12:01 +0200

    Create new function to return ouptout

--- json-tidy/magical_program.js
+++ json-tidy/magical_program.js
@@ -1,13 +1,11 @@
-console.log("Hello world");
 const fs = require("fs");
+let pathArgs = process.argv.slice (2);
 
-const myData = fs.readFileSync("./my_dirty_example.json", {
-  encoding: "utf-8",
-  flag: "r",
-});
-let result = JSON.parse(myData); // Traduire mes données
-let newData = JSON.stringify(result, null, 2); // Le mettre en forme
-fs.writeFileSync("output.json", newData); // WriteFileSync permet de mettre en forme dans un nouveau fichier (outpout.json) en lisant my dirty.json
-console.log(newData);
-const myArgs = process.argv.slice (2);
-console.log(myArgs);
+function printPath(pathArgs) {
+    
+    const myData = fs.readFileSync(pathArgs, { encoding: "utf-8",flag: "r",});
+    let result = JSON.parse(myData);// Traduire mes données
+    let newData = JSON.stringify(result, null, 2); // Le mettre en forme
+    fs.writeFileSync("output.json", newData); // WriteFileSync permet de mettre en forme dans un nouveau fichier (outpout.json) en lisant my dirty.json
+}
+printPath(pathArgs[0])
